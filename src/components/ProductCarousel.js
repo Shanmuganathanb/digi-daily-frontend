9f66d34a4150964df05b2bb58b926ebb964a822f
bshanmuganathan <shanmuganathan.b@bijak.in>
2022-03-09 10:25:03 +0530

    Added images directly in FE

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -9,22 +9,33 @@ import { listTopProducts } from '../actions/productActions'
 const ProductCarousel = () => {
   const dispatch = useDispatch()
 
-  const productTopRated = useSelector((state) => state.productTopRated)
-  const { loading, error, products } = productTopRated
+  // const productTopRated = useSelector((state) => state.productTopRated)
+  // const { loading, error, products } = productTopRated
 
   useEffect(() => {
     dispatch(listTopProducts())
   }, [dispatch])
 
-  return loading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant='danger'>{error}</Message>
-  ) : (
-        <Carousel pause='hover' className='bg-dark carousel rounded'>
-          {products.map((product) => (
+  // return loading ? (
+  //   <Loader />
+  // ) : error ? (
+  return (
+    <Carousel pause='hover' className='bg-dark carousel rounded'>
+      <Carousel.Item>
+        <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/01.jpg`} alt={'Slider Images'} fluid />
+        <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight:'300px' }} src={`/images/011.jpg`} alt={'Slider Images'} fluid/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/02.jpg`} alt={'Slider Images'} fluid />
+        <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight:'300px' }} src={`/images/022.jpg`} alt={'Slider Images'} fluid/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/03.jpg`} alt={'Slider Images'} fluid />
+        <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight:'300px' }} src={`/images/033.jpg`} alt={'Slider Images'} fluid/>
+      </Carousel.Item>
+          {/* {products.map((product) => (
             <Carousel.Item key={product._id}>
-              <Image style={{ width: "100%", maxHeight:'300px' }} src={product.image} alt={product.name} fluid/>
+              <Image style={{ width: "100%", maxHeight:'300px' }} src={product.image} alt={product.name} fluid/> */}
               {/* <Link to={`/product/${product._id}`}>
                 <Image src={product.image} alt={product.name} fluid/> */}
                 {/* <Carousel.Caption className='carousel-caption'>
@@ -33,8 +44,8 @@ const ProductCarousel = () => {
                   </h2>
                 </Carousel.Caption> */}
               {/* </Link> */}
-            </Carousel.Item>
-          ))}
+            {/* </Carousel.Item>
+          ))} */}
         </Carousel>
   )
 }
