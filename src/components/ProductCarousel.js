d083bf7a92f6c5e1be2183c34cc6d18a8e250bc7
bshanmuganathan <shanmuganathan.b@bijak.in>
2022-03-09 11:29:12 +0530

    Added Link to carousel and image size reduced

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -22,16 +22,22 @@ const ProductCarousel = () => {
   return (
     <Carousel pause='hover' className='bg-dark carousel rounded'>
       <Carousel.Item>
-        <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/01.jpg`} alt={'Slider Images'} fluid />
-        <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight:'300px' }} src={`/images/011.jpg`} alt={'Slider Images'} fluid/>
+        <Link to={`/product/622823c10606770004e884c9`}>
+          <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/01.jpg`} alt={'Slider Images'} fluid />
+          <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight: '300px' }} src={`/images/011.jpg`} alt={'Slider Images'} fluid />
+        </Link>
       </Carousel.Item>
       <Carousel.Item>
-        <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/02.jpg`} alt={'Slider Images'} fluid />
-        <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight:'300px' }} src={`/images/022.jpg`} alt={'Slider Images'} fluid/>
+        <Link to={`/product/622827390606770004e8852b`}>
+          <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/02.jpg`} alt={'Slider Images'} fluid />
+          <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight: '300px' }} src={`/images/022.jpg`} alt={'Slider Images'} fluid />
+          </Link>
       </Carousel.Item>
       <Carousel.Item>
-        <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/03.jpg`} alt={'Slider Images'} fluid />
-        <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight:'300px' }} src={`/images/033.jpg`} alt={'Slider Images'} fluid/>
+        <Link to={`/product/622826b40606770004e88519`}>
+          <Image className='d-none d-sm-block' style={{ width: "100%", maxHeight: '300px' }} src={`/images/03.jpg`} alt={'Slider Images'} fluid />
+          <Image className='d-block d-sm-none' style={{ width: "100%", maxHeight: '300px' }} src={`/images/033.jpg`} alt={'Slider Images'} fluid />
+        </Link>
       </Carousel.Item>
           {/* {products.map((product) => (
             <Carousel.Item key={product._id}>
